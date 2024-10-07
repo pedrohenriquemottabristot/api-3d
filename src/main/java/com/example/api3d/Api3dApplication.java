d4f155b0c29dcf97dd0ad0e23f8d41b617d5b189
pedrohenriquemottabristot <pedro_bristot@estudante.sesisenai.org.br>
2024-10-07 20:52:15 -0300

    first commit

---
+++ src/main/java/com/example/api3d/Api3dApplication.java
@@ -0,0 +1,13 @@
+package com.example.api3d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Api3dApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Api3dApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/api3d/Api3dApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api3d;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Api3dApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
